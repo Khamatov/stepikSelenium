0fdbbbef52acf766236e5b988440adabc484608e
@Azat_qa <spirid2398@gmail.com>
2022-11-17 19:17:42 +0300

    Найс я понял PyTest, i think

---
+++ test_abs.py
@@ -0,0 +1,9 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
