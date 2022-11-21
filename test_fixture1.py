8006d56937cc33ae53970666a2c1cfb3bb27c20c
@Azat_qa <spirid2398@gmail.com>
2022-11-21 19:02:08 +0300

    Найс я понял PyTest, i think...

--- test_fixture1.py
+++ test_fixture1.py
@@ -3,25 +3,25 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-class TestMainPage1():
-
-    @classmethod
-    def setup_class(self):
-        print("start browser for test suite..")
-        self.browser = webdriver.Chrome()
-
-    @classmethod
-    def teardown_class(self):
-        print("quit browser for test suite..")
-        self.browser.quit()
-
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+# class TestMainPage1():
+#
+#     @classmethod
+#     def setup_class(self):
+#         print("start browser for test suite..")
+#         self.browser = webdriver.Chrome()
+#
+#     @classmethod
+#     def teardown_class(self):
+#         print("quit browser for test suite..")
+#         self.browser.quit()
+#
+#     def test_guest_should_see_login_link(self):
+#         self.browser.get(link)
+#         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+#
+#     def test_guest_should_see_basket_link_on_the_main_page(self):
+#         self.browser.get(link)
+#         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
 
 class TestMainPage2():
