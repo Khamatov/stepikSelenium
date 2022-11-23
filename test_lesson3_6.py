57a5fb09e53845f5bbab7e225ec757f5b1e5adb3
@Azat_qa <spirid2398@gmail.com>
2022-11-23 18:41:37 +0300

    23.11.22

--- test_lesson3_6.py
+++ test_lesson3_6.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,13 +12,14 @@ def browser():
     browser.maximize_window()
     yield browser
     print("\nquit browser..")
+    time.sleep(5)
     browser.quit()
 #, "236896", "236897", "236898", "236899", "236903", "236904", "236905"
 @pytest.mark.parametrize("sublink",["236895"])
 def test_for_men_part_one(browser, sublink):
     link = f"https://stepik.org/lesson/{sublink}/step/1"
     browser.get(link)
-    browser.find_element(By.ID, 'ember224').click() ## click for "to come in" button
-    browser.find_element(By.NAME, "login").send_keys('123')
-    browser.find_element(By.NAME, "password").send_keys('1223')
-    # browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    browser.find_element(By.CSS_SELECTOR, '.navbar__auth_login').click() ## click for "to come in" button
+    browser.find_element(By.NAME, "login").send_keys('')
+    browser.find_element(By.NAME, "password").send_keys('')
+    browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
